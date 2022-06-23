1f6b970dfbce6b2c014ee2c7542262b564450446
Fabio-F16 <fipsfabio95@gmail.com>
2022-06-23 16:23:32 +0200

    Parziale

--- Program.cs
+++ Program.cs
@@ -10,19 +10,61 @@
 
 
 
-            for (int i = 0; i < cibiPreferiti.Length; i++)
-            {
-                Console.WriteLine(i + " " + cibiPreferiti[i]);
-            }
-
-
-
 
             Console.WriteLine("La lunghezza dei miei piatti preferiti è " + cibiPreferiti.Length);
 
             Console.WriteLine("Il mio miglior piatto preferito è " + cibiPreferiti[0]);
 
             Console.WriteLine("Il mio peggior piatto preferito è " + cibiPreferiti[cibiPreferiti.Length - 1]);
+
+            //  Console.WriteLine("Inserisci il tuo cibo preferito:");
+            //  string userFood = Console.ReadLine();
+            string userFood = "";
+            bool exist = true;
+            do
+            {
+
+                Console.WriteLine("Inserisci il tuo cibo preferito:");
+                userFood = Console.ReadLine();
+                exist = false;
+            } while (!exist);
+
+
+
+            int foodLength = cibiPreferiti.Length;
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(foodLength);
+
+            Console.WriteLine("numero random cpu " + randomNumber);
+            for (int i = 0; i < cibiPreferiti.Length; i++)
+            {
+                var foodCpu = cibiPreferiti[randomNumber-1];
+                if(cibiPreferiti[i] == userFood && cibiPreferiti[i] == foodCpu)
+                {
+                    Console.WriteLine((i + 1) + " " + cibiPreferiti[i] + " -> è il piatto preferito di entrambi");
+                   // Console.WriteLine((i + 1) + " " + cibiPreferiti[i]);
+                }
+                else if(cibiPreferiti[i] == userFood)
+                {
+                    Console.WriteLine((i + 1) + " " + cibiPreferiti[i] + " -> è il piatto preferito dell'utente");
+                }
+                else if(cibiPreferiti[i] == foodCpu)
+                {
+                    Console.WriteLine((i + 1) + " " + cibiPreferiti[i] + " -> è il piatto preferito del computer");
+                   // Console.WriteLine((i + 1) + " " + cibiPreferiti[i]);
+                }
+                else
+                {
+                    Console.WriteLine((i + 1) + " " + cibiPreferiti[i]);
+                }
+               
+            }
+
+     
+
+        
+        
+
         }
     }
 }
