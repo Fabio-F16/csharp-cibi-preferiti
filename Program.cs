39eed90464ff1b3d7c0184e2a72a35f68b7c4675
Fabio-F16 <fipsfabio95@gmail.com>
2022-06-23 15:18:26 +0200

    Ex done

--- Program.cs
+++ Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            string[] cibiPreferiti = { "Lasagne", "Pizza", "Sushi", "McDonalds", "Kebab", "Gelato", "Risotto", "Minestrone" };
+
+
+
+            for (int i = 0; i < cibiPreferiti.Length; i++)
+            {
+                Console.WriteLine(i + " " + cibiPreferiti[i]);
+            }
+
+
+
+
+            Console.WriteLine("La lunghezza dei miei piatti preferiti è " + cibiPreferiti.Length);
+
+            Console.WriteLine("Il mio miglior piatto preferito è " + cibiPreferiti[0]);
+
+            Console.WriteLine("Il mio peggior piatto preferito è " + cibiPreferiti[cibiPreferiti.Length - 1]);
         }
     }
 }
